d8a295fb58d7b60ce40ce083cc6a52d1e734ea86
Sulthan Nizarudin <n.sultan001@gmail.com>
2020-10-08 08:54:55 +0530

    added files

---
+++ todoproj/todo/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class Task(models.Model):
+    task = models.CharField(max_length=50)
+    desc = models.CharField(max_length=50)
---
+++ todoproj/todo/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+def index(request):
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    tasks = Task.objects.all()
+    context = {'form':form,'tasks':tasks}
+    return render(request,'todo/index.html',context)
+
+def delete_view(request,id):
+    tasks =Task.objects.get(id=id)
+    tasks.delete()
+    return redirect('/')
